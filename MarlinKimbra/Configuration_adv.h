041df5f0c58c1729a1f20bb48054f512ddab688d
MagoKimbra <magokimbra@hotmail.com>
2014-12-16 13:58:32 +0100

    Update Configuration_adv.h

--- MarlinKimbra/Configuration_adv.h
+++ MarlinKimbra/Configuration_adv.h
@@ -85,12 +85,12 @@
 // extruder temperature is above/below EXTRUDER_AUTO_FAN_TEMPERATURE.
 // Multiple extruders can be assigned to the same pin in which case
 // the fan will turn on when any selected extruder is above the threshold.
-#define EXTRUDER_0_AUTO_FAN_PIN   -1
-#define EXTRUDER_1_AUTO_FAN_PIN   -1
-#define EXTRUDER_2_AUTO_FAN_PIN   -1
-#define EXTRUDER_3_AUTO_FAN_PIN   -1
+#define EXTRUDER_0_AUTO_FAN_PIN -1
+#define EXTRUDER_1_AUTO_FAN_PIN -1
+#define EXTRUDER_2_AUTO_FAN_PIN -1
+#define EXTRUDER_3_AUTO_FAN_PIN -1
 #define EXTRUDER_AUTO_FAN_TEMPERATURE 50
-#define EXTRUDER_AUTO_FAN_SPEED   255
+#define EXTRUDER_AUTO_FAN_SPEED 255
 
 //===========================================================================
 //=============================Mechanical Settings===========================
