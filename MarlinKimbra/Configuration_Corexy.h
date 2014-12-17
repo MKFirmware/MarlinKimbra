f762e966b3060f1e1b43b4bc4b8a278b37290342
MagoKimbra <magokimbra@hotmail.com>
2014-12-17 15:18:18 +0100

    Update Configuration_Corexy.h

--- MarlinKimbra/Configuration_Corexy.h
+++ MarlinKimbra/Configuration_Corexy.h
@@ -20,6 +20,7 @@
 // #define ENDSTOPPULLUP_XMIN
 // #define ENDSTOPPULLUP_YMIN
 // #define ENDSTOPPULLUP_ZMIN
+// #define ENDSTOPPULLUP_EMIN
 #endif
 
 #ifdef ENDSTOPPULLUPS
@@ -29,15 +30,17 @@
 #define ENDSTOPPULLUP_XMIN
 #define ENDSTOPPULLUP_YMIN
 #define ENDSTOPPULLUP_ZMIN
+#define ENDSTOPPULLUP_EMIN
 #endif
 
 // The pullups are needed if you directly connect a mechanical endswitch between the signal and ground pins.
-const bool X_MIN_ENDSTOP_INVERTING = false; // set to true to invert the logic of the endstop.
-const bool Y_MIN_ENDSTOP_INVERTING = false; // set to true to invert the logic of the endstop.
-const bool Z_MIN_ENDSTOP_INVERTING = false; // set to true to invert the logic of the endstop.
-const bool X_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic of the endstop.
-const bool Y_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic of the endstop.
-const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic of the endstop.
+const bool X_MIN_ENDSTOP_INVERTING = false;      // set to true to invert the logic of the endstop.
+const bool Y_MIN_ENDSTOP_INVERTING = false;      // set to true to invert the logic of the endstop.
+const bool Z_MIN_ENDSTOP_INVERTING = false;      // set to true to invert the logic of the endstop.
+const bool E_MIN_ENDSTOP_INVERTING = false;      // set to true to invert the logic of the endstop.
+const bool X_MAX_ENDSTOP_INVERTING = false;      // set to true to invert the logic of the endstop.
+const bool Y_MAX_ENDSTOP_INVERTING = false;      // set to true to invert the logic of the endstop.
+const bool Z_MAX_ENDSTOP_INVERTING = false;      // set to true to invert the logic of the endstop.
 #define DISABLE_MAX_ENDSTOPS
 //#define DISABLE_MIN_ENDSTOPS
 
@@ -50,28 +53,29 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 #define X_ENABLE_ON 0
 #define Y_ENABLE_ON 0
 #define Z_ENABLE_ON 0
-#define E_ENABLE_ON 0 // For all extruders
+#define E_ENABLE_ON 0      // For all extruders
 
 // Disables axis when it's not being used.
 #define DISABLE_X false
 #define DISABLE_Y false
 #define DISABLE_Z false
-#define DISABLE_E false // For all extruders
-#define DISABLE_INACTIVE_EXTRUDER true //disable only inactive extruders and keep active extruder enabled
+#define DISABLE_E false      // For all extruders
+#define DISABLE_INACTIVE_EXTRUDER false //disable only inactive extruders and keep active extruder enabled
 
-#define INVERT_X_DIR true    // for Mendel set to false, for Orca set to true
-#define INVERT_Y_DIR false    // for Mendel set to true, for Orca set to false
-#define INVERT_Z_DIR true     // for Mendel set to false, for Orca set to true
-#define INVERT_E0_DIR false   // for direct drive extruder v9 set to true, for geared extruder set to false
-#define INVERT_E1_DIR false    // for direct drive extruder v9 set to true, for geared extruder set to false
-#define INVERT_E2_DIR false   // for direct drive extruder v9 set to true, for geared extruder set to false
-#define INVERT_E3_DIR false   // for direct drive extruder v9 set to true, for geared extruder set to false
+#define INVERT_X_DIR false      // for Mendel set to false, for Orca set to true
+#define INVERT_Y_DIR false      // for Mendel set to true, for Orca set to false
+#define INVERT_Z_DIR false      // for Mendel set to false, for Orca set to true
+#define INVERT_E0_DIR false     // for direct drive extruder v9 set to true, for geared extruder set to false
+#define INVERT_E1_DIR false     // for direct drive extruder v9 set to true, for geared extruder set to false
+#define INVERT_E2_DIR false     // for direct drive extruder v9 set to true, for geared extruder set to false
+#define INVERT_E3_DIR false     // for direct drive extruder v9 set to true, for geared extruder set to false
 
 // ENDSTOP SETTINGS:
 // Sets direction of endstops when homing; 1=MAX, -1=MIN
 #define X_HOME_DIR -1
 #define Y_HOME_DIR -1
 #define Z_HOME_DIR -1
+#define E_HOME_DIR -1
 
 #define min_software_endstops true // If true, axis won't move to coordinates less than HOME_POS.
 #define max_software_endstops true  // If true, axis won't move to coordinates greater than the defined lengths below.
@@ -83,22 +87,25 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 #define Y_MIN_POS 0
 #define Z_MAX_POS 200
 #define Z_MIN_POS 0
+#define E_MIN_POS 0
 
 #define X_MAX_LENGTH (X_MAX_POS - X_MIN_POS)
 #define Y_MAX_LENGTH (Y_MAX_POS - Y_MIN_POS)
 #define Z_MAX_LENGTH (Z_MAX_POS - Z_MIN_POS)
 
+
+//============================= Bed Leveling ===========================
+
+//#define ENABLE_AUTO_BED_LEVELING // Delete the comment to enable
+//#define Z_PROBE_REPEATABILITY_TEST  // Delete the comment to enable
+
 // set the rectangle in which to probe in manual or automatic
-#define LEFT_PROBE_BED_POSITION 15
-#define RIGHT_PROBE_BED_POSITION 185
-#define BACK_PROBE_BED_POSITION 185
-#define FRONT_PROBE_BED_POSITION 15
+#define LEFT_PROBE_BED_POSITION 20
+#define RIGHT_PROBE_BED_POSITION 180
+#define BACK_PROBE_BED_POSITION 180
+#define FRONT_PROBE_BED_POSITION 20
 
-#define XY_TRAVEL_SPEED 10000         // X and Y axis travel speed between probes, in mm/min
-//============================= Bed Auto Leveling ===========================
-
-//#define ENABLE_AUTO_BED_LEVELING // Delete the comment to enable (remove // at the start of the line)
-//#define Z_PROBE_REPEATABILITY_TEST  // Delete the comment to enable (remove // at the start of the line)
+#define XY_TRAVEL_SPEED 8000     // X and Y axis travel speed between probes, in mm/min
 
 #ifdef ENABLE_AUTO_BED_LEVELING
 
@@ -124,7 +131,6 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 // I wouldn't see a reason to go above 3 (=9 probing points on the bed)
 #define AUTO_BED_LEVELING_GRID_POINTS 2
 
-
 #else  // not AUTO_BED_LEVELING_GRID
 // with no grid, just probe 3 arbitrary points.  A simple cross-product
 // is used to esimate the plane of the print bed
@@ -142,7 +148,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 // these are the offsets to the probe relative to the extruder tip (Hotend - Probe)
 #define X_PROBE_OFFSET_FROM_EXTRUDER 0
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 20
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -3.10
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -3.70
 
 #define Z_RAISE_BEFORE_HOMING 10       // (in mm) Raise Z before homing (G28) for Probe Clearance.
 // Be sure you have this distance over your Z_MAX_POS in case
@@ -155,20 +161,20 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 
 //If defined, the Probe servo will be turned on only during movement and then turned off to avoid jerk
 //The value is the delay to turn the servo off after powered on - depends on the servo speed; 300ms is good value, but you can try lower it.
-// You MUST HAVE the SERVO_ENDSTOPS defined to use here a value higher than zero otherwise your code will not compile.
+// You MUST HAVE the SERVO ENDSTOPS defined to use here a value higher than zero otherwise your code will not compile.
 
-//  #define PROBE_SERVO_DEACTIVATION_DELAY 300
+#define PROBE_SERVO_DEACTIVATION_DELAY 300
 
 
 //If you have enabled the Bed Auto Leveling and are using the same Z Probe for Z Homing,
 //it is highly recommended you let this Z_SAFE_HOMING enabled!!!
 
 #define Z_SAFE_HOMING   // This feature is meant to avoid Z homing with probe outside the bed area.
-// When defined, it will:
-// - Allow Z homing only after X and Y homing AND stepper drivers still enabled
-// - If stepper drivers timeout, it will need X and Y homing again before Z homing
-// - Position the probe in a defined XY point before Z Homing when homing all axis (G28)
-// - Block Z homing only when the probe is outside bed area.
+                        // When defined, it will:
+                        // - Allow Z homing only after X and Y homing AND stepper drivers still enabled
+                        // - If stepper drivers timeout, it will need X and Y homing again before Z homing
+                        // - Position the probe in a defined XY point before Z Homing when homing all axis (G28)
+                        // - Block Z homing only when the probe is outside bed area.
 
 #ifdef Z_SAFE_HOMING
 
@@ -189,20 +195,19 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 #define MANUAL_X_HOME_POS 0
 #define MANUAL_Y_HOME_POS 0
 #define MANUAL_Z_HOME_POS 0
-//#define MANUAL_Z_HOME_POS 402 // For delta: Distance between nozzle and print surface after homing.
 
 //// MOVEMENT SETTINGS
 #define NUM_AXIS 4 // The axis order in all axis related arrays is X, Y, Z, E
-#define HOMING_FEEDRATE {70*60, 70*60, 2*60, 0}  // set the homing speeds (mm/min)
+#define HOMING_FEEDRATE {100*60,100*60,2*60,0}      // set the homing speeds (mm/min)
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT     {80.54,80.54,1600,495,495,495,495}    // X, Y, Z, E0, E1, E2, E3
-#define DEFAULT_MAX_FEEDRATE            {300,300,4,45,45,45,45}               // X, Y, Z, E0, E1, E2, E3 (mm/sec)
-#define DEFAULT_RETRACTION_MAX_FEEDRATE {80,80,80,80}                         // E0, E1, E2, E3 (mm/sec)
-#define DEFAULT_MAX_ACCELERATION        {5000,5000,50,5000}                   // X, Y, Z, E maximum start speed for accelerated moves.
+#define DEFAULT_AXIS_STEPS_PER_UNIT     {80,80,3200,625,625,625,625}    // X, Y, Z, E0, E1, E2, E3 default steps per unit
+#define DEFAULT_MAX_FEEDRATE            {300,300,2,100,100,100,100}     // X, Y, Z, E0, E1, E2, E3 (mm/sec)
+#define DEFAULT_RETRACTION_MAX_FEEDRATE {110,110,110,110}               // E0, E1, E2, E3 (mm/sec)
+#define DEFAULT_MAX_ACCELERATION        {3000,3000,50,1000}             // X, Y, Z, E maximum start speed for accelerated moves.
 
-#define DEFAULT_ACCELERATION          3000    // X, Y, Z and E max acceleration in mm/s^2 for printing moves
+#define DEFAULT_ACCELERATION          2500    // X, Y, Z and E max acceleration in mm/s^2 for printing moves
 #define DEFAULT_RETRACT_ACCELERATION  3000    // X, Y, Z and E max acceleration in mm/s^2 for retracts
 
 // Offset of the extruders (uncomment if using more than one and relying on firmware to position when changing).
@@ -212,6 +217,6 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 // #define EXTRUDER_OFFSET_Y {0.0, 5.00}  // (in mm) for each extruder, offset of the hotend on the Y axis
 
 // The speed change that does not require acceleration (i.e. the software might assume it can be done instantaneously)
-#define DEFAULT_XYJERK                10.0    // (mm/sec)
-#define DEFAULT_ZJERK                 0.4     // (mm/sec)
-#define DEFAULT_EJERK                 5.0    // (mm/sec)
+#define DEFAULT_XYJERK 10.0    // (mm/sec)
+#define DEFAULT_ZJERK 0.4      // (mm/sec)
+#define DEFAULT_EJERK 5.0      // (mm/sec)
