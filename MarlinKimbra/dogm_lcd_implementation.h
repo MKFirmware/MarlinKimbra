40b2278f4f074317f96d77063aa2c3bea4d895c2
MagoKimbra <magokimbra@hotmail.com>
2015-02-09 22:22:33 +0100

    Put volumetric in filament dia

--- MarlinKimbra/dogm_lcd_implementation.h
+++ MarlinKimbra/dogm_lcd_implementation.h
@@ -276,7 +276,7 @@ static void lcd_implementation_status_screen() {
       lcd_printPGM(PSTR("dia:"));
       u8g.print(ftostr12ns(filament_width_meas));
       lcd_printPGM(PSTR(" factor:"));
-      u8g.print(itostr3(extrudemultiply));
+      u8g.print(itostr3(volumetric_multiplier[active_extruder]));
       u8g.print('%');
     }
   #endif
