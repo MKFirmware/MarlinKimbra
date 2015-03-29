924fef1e6dbda43105a4703cd513cd286b56d942
Simone Primarosa <simone.pri@hotmail.it>
2015-03-29 20:32:24 +0200

    oozing prevention improvements
    Il filamento viene ri-estruso poco prima che l'estrusore diventi freddo

--- MarlinKimbra/Marlin_main.cpp
+++ MarlinKimbra/Marlin_main.cpp
@@ -6516,11 +6516,21 @@ void manage_inactivity(bool ignore_stepper_queue/*=false*/) //default argument s
   #endif
 
   #ifdef IDLE_OOZING_PREVENT
-    if (!debugDryrun() && !axis_is_moving && (millis() - axis_last_activity) >  IDLE_OOZING_SECONDS * 1000 && degHotend(active_extruder) > IDLE_OOZING_MINTEMP) {
+    if (degHotend(active_extruder) > IDLE_OOZING_MINTEMP && !debugDryrun() && !axis_is_moving)
+	{
       #ifdef FILAMENTCHANGEENABLE
         if (!filament_changing)
       #endif
-      IDLE_OOZING_retract(true);
+      {
+        if(degHotend(active_extruder) < IDLE_OOZING_MAXTEMP && degTargetHotend(active_extruder) < IDLE_OOZING_MINTEMP)
+        {
+          IDLE_OOZING_retract(false);
+        }
+        else if((millis() - axis_last_activity) >  IDLE_OOZING_SECONDS*1000)
+        {
+          IDLE_OOZING_retract(true);
+        }
+      }
     }
   #endif
 
