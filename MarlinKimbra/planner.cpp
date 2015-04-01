19de031b18059168d6b15147c2bcfa59ada9a1f8
Simone Primarosa <simone.pri@hotmail.it>
2015-04-01 23:03:53 +0200

    Code clean

--- MarlinKimbra/planner.cpp
+++ MarlinKimbra/planner.cpp
@@ -429,7 +429,7 @@ void check_axes_activity() {
     disable_e3();
   }
 
-  #if defined(FAN_PIN) && FAN_PIN > -1 // HAS_FAN
+  #if HAS_FAN // HAS_FAN
     #ifdef FAN_KICKSTART_TIME
       static unsigned long fan_kick_end;
       if (tail_fan_speed) {
@@ -449,7 +449,7 @@ void check_axes_activity() {
     #else
       analogWrite(FAN_PIN, tail_fan_speed);
     #endif //!FAN_SOFT_PWM
-  #endif //FAN_PIN > -1
+  #endif //HAS_FAN
 
   #ifdef AUTOTEMP
     getHighESpeed();
