c703141cc6a23843e488bbd7d9ffa09ee2dd3dc4
MagoKimbra <magokimbra@hotmail.com>
2015-05-24 21:47:50 +0200

    Update conditionals.h

--- MarlinKimbra/conditionals.h
+++ MarlinKimbra/conditionals.h
@@ -221,6 +221,23 @@
 
   #define CONDITIONALS_H
 
+  /**
+   * SINGLENOZZLE
+   */
+  #ifdef SINGLENOZZLE
+    #define HOTENDS 1
+    #undef TEMP_SENSOR_1_AS_REDUNDANT
+  #else
+    #define HOTENDS EXTRUDERS
+  #endif
+
+  /**
+   * DRIVER_EXTRUDERS
+   */
+  #if !defined(MKR4) && !defined(NPR2)
+    #define DRIVER_EXTRUDERS EXTRUDERS // This defines the number of Driver extruder
+  #endif
+
   #ifndef __SAM3X8E__
     #ifndef AT90USB
       #define HardwareSerial_h // trick to disable the standard HWserial
@@ -278,23 +295,6 @@
   #ifdef FIRMWARE_TEST
     #undef BAUDRATE
     #define BAUDRATE 115200  // Baudrate setting to 115200 because serial monitor arduino function at max 115200 baudrate.
-  #endif
-
-  /**
-   * SINGLENOZZLE
-   */
-  #ifdef SINGLENOZZLE
-    #define HOTENDS 1
-    #undef TEMP_SENSOR_1_AS_REDUNDANT
-  #else
-    #define HOTENDS EXTRUDERS
-  #endif
-
-  /**
-   * DRIVER_EXTRUDERS
-   */
-  #if !defined(MKR4) && !defined(NPR2)
-    #define DRIVER_EXTRUDERS EXTRUDERS // This defines the number of Driver extruder
   #endif
 
   /**
