c495ec3c8a1d7126680b7e1f979f383cdf183a1f
MagoKimbra <magokimbra@hotmail.com>
2015-10-02 19:36:35 +0200

    Same fix

--- MarlinKimbra/pins.h
+++ MarlinKimbra/pins.h
@@ -2249,9 +2249,7 @@
 #define ORIG_TEMP_0_PIN          6   // ANALOG NUMBERING
 #define ORIG_TEMP_1_PIN          5   // 2    // ANALOG NUMBERING
 #define ORIG_TEMP_2_PIN          4   // 3     // ANALOG NUMBERING
-
-#define ORIG_TEMP_3_PIN         -1   // ANALOG NUMBERING
-#define TEMP_4_PIN              -1   // ANALOG NUMBERING
+#define ORIG_TEMP_3_PIN          3   // ANALOG NUMBERING
 
 #if NUM_SERVOS > 0
   #define SERVO0_PIN            11
@@ -2381,20 +2379,20 @@
 #define ORIG_X_STEP_PIN         54
 #define ORIG_X_DIR_PIN          55
 #define ORIG_X_ENABLE_PIN       38
-#define ORIG_X_MIN_PIN               3
-#define ORIG_X_MAX_PIN               2
+#define ORIG_X_MIN_PIN           3
+#define ORIG_X_MAX_PIN           2
 
 #define ORIG_Y_STEP_PIN         60
 #define ORIG_Y_DIR_PIN          61
 #define ORIG_Y_ENABLE_PIN       56
-#define ORIG_Y_MIN_PIN               14
-#define ORIG_Y_MAX_PIN               15
+#define ORIG_Y_MIN_PIN          14
+#define ORIG_Y_MAX_PIN          15
 
 #define ORIG_Z_STEP_PIN         46
 #define ORIG_Z_DIR_PIN          48
 #define ORIG_Z_ENABLE_PIN       62
-#define ORIG_Z_MIN_PIN               18
-#define ORIG_Z_MAX_PIN               19
+#define ORIG_Z_MIN_PIN          18
+#define ORIG_Z_MAX_PIN          19
 
 #define Y2_STEP_PIN             36
 #define Y2_DIR_PIN              34
@@ -2412,6 +2410,18 @@
 #define ORIG_E1_DIR_PIN         34
 #define ORIG_E1_ENABLE_PIN      30
 
+#define ORIG_HEATER_0_PIN       10
+#define ORIG_HEATER_1_PIN       -1
+#define ORIG_HEATER_2_PIN       -1
+#define ORIG_HEATER_BED_PIN      8    // BED
+
+#define ORIG_TEMP_0_PIN          9   // ANALOG NUMBERING
+#define ORIG_TEMP_1_PIN         -1   // ANALOG NUMBERING
+#define ORIG_TEMP_2_PIN         -1   // ANALOG NUMBERING
+#define ORIG_TEMP_BED_PIN       10   // ANALOG NUMBERING
+
+#define ORIG_FAN_PIN             9
+#define PS_ON_PIN               12
 #define SDPOWER                 -1
 #define SDSS                    53
 #define LED_PIN                 13
