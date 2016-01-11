16ad18c4aac564771c09a0deb892e2d94911e800
MagoKimbra <magokimbra@hotmail.com>
2016-01-12 00:18:26 +0100

    New name

--- MarlinKimbra/Configuration_Version.h
+++ MarlinKimbra/Configuration_Version.h
@@ -2,8 +2,8 @@
   #define CONFIGURATION_VERSION_H
 
   #define BUILD_VERSION "MarlinKimbra 4.2.4 dev"
-  #define SHORT_BUILD_VERSION "4.2.4 dev"
+  #define SHORT_BUILD_VERSION "4.2.4_dev"
   #define STRING_DISTRIBUTION_DATE __DATE__ " " __TIME__    // build date and time
   // It might also be appropriate to define a location where additional information can be found
-  #define SOURCE_CODE_URL  "https://github.com/MagoKimbra/MarlinKimbra"
+  #define FIRMWARE_URL  "https://github.com/MagoKimbra/MarlinKimbra"
 #endif
