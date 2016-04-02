0009208066d720f20bde858ce419409a9ebd174a
Franco (nextime) Lanza <franco@nexlab.it>
2016-04-02 05:44:02 +0200

    Add M/G codes to the docs in MK.ino

--- MK/MK.ino
+++ MK/MK.ino
@@ -34,11 +34,13 @@
  *
  * "G" Codes
  *
- * G0  -> G1
- * G1  - Coordinated Movement X Y Z E
+ * G0  -> G1 except for laser where G0 is "move without firing"
+ * G1  - Coordinated Movement X Y Z E, for laser move by firing
  * G2  - CW ARC
  * G3  - CCW ARC
  * G4  - Dwell S<seconds> or P<milliseconds>
+ * G5  - Bezier curve - from http://forums.reprap.org/read.php?147,93577
+ * G7  - Execute laser raster line
  * G10 - retract filament according to settings of M207
  * G11 - retract recover filament according to settings of M208
  * G28 - Home one or more axes
@@ -170,6 +172,9 @@
  * M595 - Set hotend AD595 O<offset> and S<gain>
  * M600 - Pause for filament change X[pos] Y[pos] Z[relative lift] E[initial retract] L[later retract distance for removal]
  * M605 - Set dual x-carriage movement mode: S<mode> [ X<duplication x-offset> R<duplication temp offset> ]
+ * M649 - laser set options
+ * M650 - mUVe peel set peel distance
+ * M651 - mUVe peel run peel move
  * M666 - Set z probe offset or Endstop and delta geometry adjustment
  * M906 - Set motor currents XYZ T0-4 E
  * M907 - Set digital trimpot motor current using axis codes.
