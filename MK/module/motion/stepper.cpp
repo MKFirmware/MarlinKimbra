7bcab335491edbc622a709bc759ef6998fde54c1
MagoKimbra <magokimbra@hotmail.com>
2016-05-27 18:26:37 +0200

    Fix

--- MK/module/motion/stepper.cpp
+++ MK/module/motion/stepper.cpp
@@ -297,11 +297,11 @@ FORCE_INLINE unsigned short calc_timer(unsigned short step_rate) {
   NOMORE(step_rate, MAX_STEP_FREQUENCY);
 
   if(step_rate > (2 * DOUBLE_STEP_FREQUENCY)) { // If steprate > 2*DOUBLE_STEP_FREQUENCY >> step 4 times
-    step_rate = (step_rate >> 2);
+    sstep_rate >>= 2;
     step_loops = 4;
   }
   else if(step_rate > DOUBLE_STEP_FREQUENCY) { // If steprate > DOUBLE_STEP_FREQUENCY >> step 2 times
-    step_rate = (step_rate >> 1);
+    step_rate >>= 1;
     step_loops = 2;
   }
   else {
