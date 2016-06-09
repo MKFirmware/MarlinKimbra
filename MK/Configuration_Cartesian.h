c714d10ecef2cafa7c2df19b3a8d7f48c3ef55b6
MagoKimbra <magokimbra@hotmail.com>
2016-06-09 21:27:54 +0200

    Fix

--- MK/Configuration_Cartesian.h
+++ MK/Configuration_Cartesian.h
@@ -1,7 +1,28 @@
-#ifndef CONFIGURATION_MECHANISM
-#define CONFIGURATION_MECHANISM
-#define KNOWN_MECH
-/*
+/**
+ * MK & MK4due 3D Printer Firmware
+ *
+ * Based on Marlin, Sprinter and grbl
+ * Copyright (C) 2011 Camiel Gubbels / Erik van der Zalm
+ * Copyright (C) 2013 - 2016 Alberto Cotronei @MagoKimbra
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * Configuration_Cartesian.h
+ *
  * This configuration file contains mechanism settings for cartesian printer.
  *
  * - Machine name
@@ -32,6 +53,11 @@
  * Feature-settings can be found in Configuration_Feature.h
  * Pins-settings can be found in "Configuration_Pins.h"
  */
+
+#ifndef CONFIGURATION_MECHANISM
+#define CONFIGURATION_MECHANISM
+
+#define KNOWN_MECH
  
 /*****************************************************************************************
  *********************************** Machine name ****************************************
--- MK/Configuration_Feature.h
+++ MK/Configuration_Feature.h
@@ -1,7 +1,28 @@
-#ifndef CONFIGURATION_FEATURE_H
-#define CONFIGURATION_FEATURE_H
+/**
+ * MK & MK4due 3D Printer Firmware
+ *
+ * Based on Marlin, Sprinter and grbl
+ * Copyright (C) 2011 Camiel Gubbels / Erik van der Zalm
+ * Copyright (C) 2013 - 2016 Alberto Cotronei @MagoKimbra
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
-/*
+/**
+ * Configuration_Feature.h
+ *
  * This configuration file contains all features that can be enabled.
  *
  * EXTRUDER FEATURES:
@@ -73,9 +94,8 @@
  * 
  */
 
-//===========================================================================
-//============================= EXTRUDER FEATURES ===========================
-//===========================================================================
+#ifndef CONFIGURATION_FEATURE_H
+#define CONFIGURATION_FEATURE_H
 
 /**************************************************************************
  **************************** Fan configuration ***************************
